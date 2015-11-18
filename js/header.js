951404d264addf797024259726b9a51657cf339d
Styrmir oli þorsteinsson <styrmir97@gmail.com>
2015-11-18 08:36:49 +0000

    changes

--- js/header.js
+++ js/header.js
@@ -1,45 +1,4 @@
-$(document).ready(function() {
-	var windowWidth = $(window).width();
-
-	$( ".cross" ).hide();
-
-	if(windowWidth < 640) {
-		$( ".header-list" ).hide();
-		$(".hamburger").show();
-	} else {
-		$(".hamburger").hide();
-		$( ".header-list" ).show();
-	}
-
-	$( ".hamburger" ).click(function() {
-		toggleMenu(true);
-	});
-
-	$( ".cross" ).click(function() {
-		toggleMenu(false);
-	});
+$('button').click(function() {
+  
+$(this).toggleClass('expanded').siblings('div').slideToggle();
 });
-
-$(document).resize(function() {
-	if(windowWidth < 640) {
-		$( ".header-list" ).hide();
-		$(".hamburger").show();
-	} else {
-		$( ".header-list" ).show();
-		$(".hamburger").hide();
-	}
-});
-
-function toggleMenu(show) {
-	if(show) {
-		$( ".header-list" ).slideToggle( "slow", function() {
-			$( ".hamburger" ).hide();
-			$( ".cross" ).show();
-		});
-	} else {
-		$( ".header-list" ).slideToggle( "slow", function() {
-			$( ".cross" ).hide();
-			$( ".hamburger" ).show();
-		});
-	}
-}
