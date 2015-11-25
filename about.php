b6d88f64ed4a7e9ece932c044370912f0fa371f3
Styrmir oli þorsteinsson <styrmir97@gmail.com>
2015-11-25 10:04:13 +0000

    about my ass

--- about.php
+++ about.php
@@ -12,7 +12,10 @@
 		<div class="main">
 			<h4 class="center-text">Tickets</h4>
 			<p class="center-text">
-				This is a side where you can get tickets to go on all then best shows in Iceland.
+				This is a side where you can get tickets to go on all then best shows in Iceland.<br>
+				Tickets is THE most amazing wepside in the world and it was just a school project.<br>
+				it got so famous that is almost kill the internet and made the creators og this side<br>
+				a multi billon dallalla company.
 			</p>
 			<p class="center-text">
 				Gul gabel gaybel
