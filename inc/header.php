bc59f4f592c43b75dadcc1e7a732d6cd8641c304
Styrmir oli þorsteinsson <styrmir97@gmail.com>
2015-11-25 09:21:06 +0000

    changed header

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,12 @@
 <header class="header">
+	<button class="hamburger">&#9776;</button>
 	<ul class="header-list">
 		<!-- Siggi hvað ertu að gera með lífið -->
 		<li class="logo"><a href="index.php" class="header-link"><img src="img/logo.png" width="100" height="200"></a></li>
 		<li><a href="index.php" class="header-link change-bg link-padding">Home</a></li>
 		<li><a href="about.php" class="header-link change-bg link-padding">About</a></li>
-		<li><a href="contact.php" class="header-link change-bg link-padding">Contact</a></li>
+		<li><a href="contact.php" class="header-link change-bg link-padding bottom-border">Contact</a></li>
+		<button class="cross">X</button>
 	</ul>
+	<script type="text/javascript" src="js/header.js"></script>
 </header>
