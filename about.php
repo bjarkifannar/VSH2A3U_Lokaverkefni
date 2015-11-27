083b37b4a10931950866b07f0f140605c2b1f827
Styrmir oli þorsteinsson <styrmir97@gmail.com>
2015-11-27 11:28:59 +0000

    changed about

--- about.php
+++ about.php
@@ -10,8 +10,8 @@
 			require "inc/header.php";
 		?>
 		<div class="main">
-			<h4 class="center-text">Tickets</h4>
-			<p class="center-text">
+			<h4 class="center-text-top">Tickets</h4>
+			<p class="center-text-top">
 				IS THIS YOU
 			</p>
 			<img src="img/tickets.jpg" class="img-text">
@@ -22,11 +22,11 @@
 				well tickets.is is here to save you
 			</p>
 			<img src="img/reykjavik.jpg" class="img-text">
-			<p class="center-text">
+			<p class="center-text-buttom">
 				This is a side where you can get tickets to go on all then best shows in Iceland.<br>
 				Tickets is THE most amazing webside in the world and it was just a school project.<br>
 				it got so famous that is almost kill the internet and made the creators og this side<br>
-				a multi billon dallalla(because it cooler) company.
+				a multi billon dallalla(because it cooler) company.<br>
 				... btw did we tell you that we sell tickets for the best prize in all of iceland
 			</p>
 		</div>
